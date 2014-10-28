3ed694dcae9e5a2673057f3316fed989d8bbcb77
Beth <beth@bethesque.com>
2014-10-29 10:32:28 +1100

    Added feature logs to git repo to help document mock service behaviour

--- spec/features/mock_multiple_responses_spec.rb
+++ spec/features/mock_multiple_responses_spec.rb
@@ -5,7 +5,14 @@ describe Pact::Consumer::MockService do
 
   include Rack::Test::Methods
 
-  let(:app) { Pact::Consumer::MockService.new(log_file: StringIO.new) }
+  MULTIPLE_LOG_PATH = File.join File.dirname(__FILE__), 'log', 'mock_multiple_responses_spec.log'
+
+  before :all do
+    FileUtils.rm MULTIPLE_LOG_PATH
+  end
+
+  let(:log_file) { File.open MULTIPLE_LOG_PATH, 'a' }
+  let(:app) { Pact::Consumer::MockService.new(log_file: log_file) }
 
   # NOTE: the admin_headers are Rack headers, they will be converted
   # to X-Pact-Mock-Service and Content-Type by the framework
@@ -48,7 +55,10 @@ describe Pact::Consumer::MockService do
         }.to_json
       end
 
-      it "returns the expected response" do
+      it "returns the expected response" do | example |
+        # Clear interactions
+        delete "/interactions?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
+
         # Set up expected interaction
         post "/interactions", expected_interaction, admin_headers
 
@@ -70,7 +80,7 @@ describe Pact::Consumer::MockService do
         expect(JSON.parse(last_response.body)).to eq([{ 'name' => 'Xena Zebra' }])
 
         # Verify
-        get "/interactions/verification", nil, admin_headers
+        get "/interactions/verification?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
         expect(last_response.status).to eq 200
 
       end
@@ -95,7 +105,10 @@ describe Pact::Consumer::MockService do
         }.to_json
       end
 
-      it "returns an error response" do
+      it "returns an error response" do | example |
+        # Clear interactions
+        delete "/interactions?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
+
         # Set up expected interaction
         post "/interactions", expected_interaction, admin_headers
 
@@ -110,7 +123,7 @@ describe Pact::Consumer::MockService do
         expect(last_response.body).to include 'Multiple interaction found'
 
         # Verification will be false
-        get "/interactions/verification", nil, admin_headers
+        get "/interactions/verification?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
         expect(last_response.status).to eq 500
         expect(last_response.body).to include 'Actual interactions do not match expected interactions'
       end
--- spec/features/mock_one_response_spec.rb
+++ spec/features/mock_one_response_spec.rb
@@ -1,11 +1,19 @@
 require 'pact/consumer/mock_service/app'
 require 'rack/test'
+require 'cgi'
 
 describe Pact::Consumer::MockService do
 
   include Rack::Test::Methods
 
-  let(:app) { Pact::Consumer::MockService.new(log_file: StringIO.new) }
+  ONE_LOG_PATH = File.join File.dirname(__FILE__), 'log', 'mock_one_response_spec.log'
+
+  before :all do
+    FileUtils.rm ONE_LOG_PATH
+  end
+
+  let(:log_file) { File.open ONE_LOG_PATH, 'a' }
+  let(:app) { Pact::Consumer::MockService.new(log_file: log_file) }
 
   # NOTE: the admin_headers are Rack headers, they will be converted
   # to X-Pact-Mock-Service and Content-Type by the framework
@@ -30,7 +38,10 @@ describe Pact::Consumer::MockService do
 
   context "when a response has been mocked" do
     context "when the actual request matches the expected request" do
-      it "returns the expected response" do
+      it "returns the expected response" do | example |
+        # Clear interactions
+        delete "/interactions?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
+
         # Set up expected interaction
         post "/interactions", expected_interaction, admin_headers
 
@@ -43,13 +54,16 @@ describe Pact::Consumer::MockService do
         expect(JSON.parse(last_response.body)).to eq([{ 'name' => 'Mary' }])
 
         # Verify that all the expected interactions were executed, and no extras were made
-        get "/interactions/verification", nil, admin_headers
+        get "/interactions/verification?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
         expect(last_response.status).to eq 200
       end
     end
 
     context "when the actual request does not match the expected request" do
-      it "returns an error response" do
+      it "returns an error response" do | example |
+        # Clear interactions
+        delete "/interactions?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
+
         # Set up expected interaction
         post "/interactions", expected_interaction, admin_headers
 
@@ -61,7 +75,7 @@ describe Pact::Consumer::MockService do
         expect(last_response.body).to include 'No interaction found'
 
         # Verification will be false
-        get "/interactions/verification", nil, admin_headers
+        get "/interactions/verification?example_description=#{CGI::escape(example.full_description)}", nil, admin_headers
         expect(last_response.status).to eq 500
         expect(last_response.body).to include 'Actual interactions do not match expected interactions'
       end
