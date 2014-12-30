727faf820c97835431cdd26bc33ad484fd80aed1
André Allavena <andre@igecorporate.com>
2014-12-30 15:58:09 +1000

    Fix comment

--- lib/pact/consumer/mock_service/candidate_options.rb
+++ lib/pact/consumer/mock_service/candidate_options.rb
@@ -3,10 +3,9 @@ require 'pact/consumer/mock_service/web_request_options'
 module Pact
   module Consumer
 
-    # Need good explanation here
     # Allow web preflight requests to the intaractions setup by the user
-    # This is only needed in a CORS setup, where the
-    # Browsers typically do a OPTIONS before a POST for cross domain requests
+    # This is only needed in a CORS setup, where the browsers do
+    # an OPTIONS call before a DELETE, POST (for most request), etc. in a cross domain requests
     class CandidateOptions < WebRequestOptions
       def initialize name, logger, cors_enabled
         super(name,logger)
@@ -20,3 +19,4 @@ module Pact
     end
   end
 end
+
