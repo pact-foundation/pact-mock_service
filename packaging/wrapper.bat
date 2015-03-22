9b3127c5c25998f21a7e3d37b2b594124ffed6db
Neil Campbell <ncampbell@seek.com.au>
2015-03-23 09:44:48 +1100

    Fixing a path issue as %~dp0 resolves the path the bat is running from

--- packaging/wrapper.bat
+++ packaging/wrapper.bat
@@ -1,8 +1,17 @@
 @echo off
 
+SET RUNNING_PATH=%~dp0
+CALL :RESOLVE "%RUNNING_PATH%\.." ROOT_PATH
+
 :: Tell Bundler where the Gemfile and gems are.
-set "BUNDLE_GEMFILE=%~dp0\lib\vendor\Gemfile"
+set "BUNDLE_GEMFILE=%ROOT_PATH%\lib\vendor\Gemfile"
 set BUNDLE_IGNORE_CONFIG=
 
 :: Run the actual app using the bundled Ruby interpreter, with Bundler activated.
-@"%~dp0\lib\ruby\bin\ruby.bat" -rbundler/setup -I%~dp0\lib\app\lib "%~dp0\lib\app\pact-mock-service.rb" %*
+@"%ROOT_PATH%\lib\ruby\bin\ruby.bat" -rbundler/setup -I%ROOT_PATH%\lib\app\lib "%ROOT_PATH%\lib\app\pact-mock-service.rb" %*
+
+GOTO :EOF
+
+:RESOLVE
+SET %2=%~f1
+GOTO :EOF
