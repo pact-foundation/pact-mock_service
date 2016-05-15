fe5ea69c8e61b7245a002418e57cdb450aa0e8ff
Matt Fellows <matt.fellows@onegeek.com.au>
2016-05-15 13:31:45 +1000

    Added -rreadline flag to ruby CLI execution wrapper

--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -22,4 +22,4 @@ export BUNDLE_GEMFILE="$LIBDIR/vendor/Gemfile"
 unset BUNDLE_IGNORE_CONFIG
 
 # Run the actual app using the bundled Ruby interpreter, with Bundler activated.
-exec "$LIBDIR/ruby/bin/ruby" -rbundler/setup -I$LIBDIR/app/lib "$LIBDIR/app/pact-mock-service.rb" $@
+exec "$LIBDIR/ruby/bin/ruby" -rreadline -rbundler/setup -I$LIBDIR/app/lib "$LIBDIR/app/pact-mock-service.rb" $@
