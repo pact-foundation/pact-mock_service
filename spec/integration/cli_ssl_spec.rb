d1ab8ff2519430ec7637bb283b7e3f5929ca2192
Bobby Earl <bobby.earl@blackbaud.com>
2016-10-21 11:29:00 -0400

    Added intergration test and updated README.md

--- spec/integration/cli_ssl_spec.rb
+++ spec/integration/cli_ssl_spec.rb
@@ -5,21 +5,27 @@ describe "The pact-mock-service command line interface, with SSL", mri_only: tru
 
   include Pact::IntegrationTestSupport
 
-  before :all do
-    @pid = start_server 4343, '--ssl', false
-  end
-
   it "should respond with SSL" do
+    @pid = start_server 4343, '--ssl', false
     wait_until_server_started 4343, true
+    kill_server @pid
   end
 
   it "sets the X-Pact-Mock-Service-Location header with https" do
+    @pid = start_server 4343, '--ssl', false
     wait_until_server_started 4343, true
     response = connect_via_ssl 4343
     expect(response.headers['X-Pact-Mock-Service-Location']).to eq 'https://0.0.0.0:4343'
-  end
-
-  after :all do
     kill_server @pid
   end
+
+  it "should accept a sslcert and sslkey" do
+    @path = "#{Dir.pwd}/spec/support/ssl/"
+    @pid = start_server 4343, '--ssl --sslcert "' + @path + 'server.crt" --sslkey "' + @path + 'server.key"', false
+    wait_until_server_started 4343, true
+    response = connect_via_ssl 4343
+    expect(response.headers['X-Pact-Mock-Service-Location']).to eq 'https://0.0.0.0:4343'
+    kill_server @pid
+  end
+
 end
