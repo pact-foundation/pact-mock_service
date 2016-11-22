ea605f17096e1ee49de89ce704572114d365f32a
Bethany Skurrie <bethesk@gmail.com>
2016-11-22 11:16:50 +1100

    Moved server kill back to after block

--- spec/integration/cli_ssl_spec.rb
+++ spec/integration/cli_ssl_spec.rb
@@ -6,27 +6,28 @@ describe "The pact-mock-service command line interface, with SSL", mri_only: tru
   include Pact::IntegrationTestSupport
 
   it "should respond with SSL" do
-    pid = start_server 4343, '--ssl', false
+    @pid = start_server 4343, '--ssl', false
     wait_until_server_started 4343, true
-    kill_server pid
   end
 
 
   it "sets the X-Pact-Mock-Service-Location header with https" do
-    pid = start_server 4343, '--ssl', false
+    @pid = start_server 4343, '--ssl', false
     wait_until_server_started 4343, true
     response = connect_via_ssl 4343
     expect(response.headers['X-Pact-Mock-Service-Location']).to eq 'https://0.0.0.0:4343'
-    kill_server pid
   end
 
   it "should accept a sslcert and sslkey" do
     path = "#{Dir.pwd}/spec/support/ssl/"
-    pid = start_server 4343, '--ssl --sslcert "' + path + 'server.crt" --sslkey "' + path + 'server.key"', false
+    @pid = start_server 4343, '--ssl --sslcert "' + path + 'server.crt" --sslkey "' + path + 'server.key"', false
     wait_until_server_started 4343, true
     response = connect_via_ssl 4343
     expect(response.headers['X-Pact-Mock-Service-Location']).to eq 'https://0.0.0.0:4343'
-    kill_server pid
+  end
+
+  after do
+    kill_server @pid
   end
 
 end
