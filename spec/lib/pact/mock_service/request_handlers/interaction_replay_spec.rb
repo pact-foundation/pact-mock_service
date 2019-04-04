a30ebe430dec03359091122c8557e7fb11ef265a
Alexander Bolshakov <lextiz@gmail.com>
2019-04-04 14:54:41 +0200

    Update interaction_replay_spec.rb

--- spec/lib/pact/mock_service/request_handlers/interaction_replay_spec.rb
+++ spec/lib/pact/mock_service/request_handlers/interaction_replay_spec.rb
@@ -146,6 +146,7 @@ module Pact
           
           context "when the body contains special charachters" do
             let(:actual_body) { '\xEB' }
+            let(:expected_response_body) { '\xEB' }
 
             it "returns the specified response status" do
               expect(response_status).to eq 200
