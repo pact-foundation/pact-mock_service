1ecad601b5b3f024b11d2aecc830ad42a8e69e14
Matt Fellows <matthew.fellows@latitudefinancial.com>
2020-01-15 16:29:23 +1100

    feat: add token, username and password options to stub service

--- lib/pact/stub_service/cli.rb
+++ lib/pact/stub_service/cli.rb
@@ -27,6 +27,9 @@ module Pact
       method_option :port, aliases: "-p", desc: "Port on which to run the service"
       method_option :host, aliases: "-h", desc: "Host on which to bind the service", default: 'localhost'
       method_option :log, aliases: "-l", desc: "File to which to log output"
+      method_option :broker_username, aliases: "-n", desc: "Pact Broker basic auth username", :required => false
+      method_option :broker_password, aliases: "-p", desc: "Pact Broker basic auth password", :required => false
+      method_option :broker_token, aliases: "-k", desc: "Pact Broker bearer token", :required => false
       method_option :log_level, desc: "Log level. Options are DEBUG INFO WARN ERROR", default: "DEBUG"
       method_option :cors, aliases: "-o", desc: "Support browser security in tests by responding to OPTIONS requests and adding CORS headers to mocked responses"
       method_option :ssl, desc: "Use a self-signed SSL cert to run the service over HTTPS", type: :boolean, default: false
