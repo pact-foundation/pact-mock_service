a0e24a23c1048d50069d38ac0b0c85242344209d
Beth Skurrie <beth@bethesque.com>
2020-01-17 13:29:48 +1100

    docs: document that you can set the broker token via an env var
    [ci-skip]

--- lib/pact/stub_service/cli.rb
+++ lib/pact/stub_service/cli.rb
@@ -29,7 +29,7 @@ module Pact
       method_option :log, aliases: "-l", desc: "File to which to log output"
       method_option :broker_username, aliases: "-n", desc: "Pact Broker basic auth username", :required => false
       method_option :broker_password, aliases: "-p", desc: "Pact Broker basic auth password", :required => false
-      method_option :broker_token, aliases: "-k", desc: "Pact Broker bearer token", :required => false
+      method_option :broker_token, aliases: "-k", desc: "Pact Broker bearer token (can also be set using the PACT_BROKER_TOKEN environment variable)", :required => false
       method_option :log_level, desc: "Log level. Options are DEBUG INFO WARN ERROR", default: "DEBUG"
       method_option :cors, aliases: "-o", desc: "Support browser security in tests by responding to OPTIONS requests and adding CORS headers to mocked responses"
       method_option :ssl, desc: "Use a self-signed SSL cert to run the service over HTTPS", type: :boolean, default: false
