c5e2311c9915a526b1600b5f0a41ed5b287b1e3f
Yousaf Nabi <you@saf.dev>
2024-06-17 15:35:44 +0100

    fix: rack 2.x dont load Rack::Handler::WEBrick class to avoid warnings
    use webbrick.rb custom handler in cli.rb

--- lib/rack/handler/webbrick.rb
+++ lib/rack/handler/webbrick.rb
@@ -2,7 +2,6 @@ module Rack
   module Handler
       begin
         require 'rack/handler/webrick'
-        WEBrick = Class.new(Rack::Handler::WEBrick)
       rescue LoadError
         require 'rackup/handler/webrick'
         WEBrick = Class.new(Rackup::Handler::WEBrick)
